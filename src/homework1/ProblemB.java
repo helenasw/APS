b2f6ed101cceee42bfa30a0dc897a754a5c9d43b
Helena Wentworth <helena.wentworth@gmail.com>
2015-02-12 12:29:11 -0500

    Attempt 2 at scheduling problem. Still wrong answer.

--- src/homework1/ProblemB.java
+++ src/homework1/ProblemB.java
@@ -7,70 +7,98 @@ import java.util.BitSet;
 import java.util.StringTokenizer;
 
 /**
- * Created by Helena on 2/6/2015.
+ * Random notes for future me:
+ * You have to process everything even if there is a conflict just so that you get to the right start of the
+ * next test case (ie don't break when you find a conflict and don't skip processing repeating even if you already
+ * found an error). Also, make sure that you're still checking for termination lines.
  */
 public class ProblemB {
+
+    private static BitSet schedule;
 
     public static void main(String[] args) throws IOException {
         final int limit = 1000000;
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
-        BitSet schedule = new BitSet(limit);
+        schedule = new BitSet(limit);
 
-        int oneTasks, repTasks, start, end, repInt, c;
+        int oneTasks, repTasks, start, end, interval;
         boolean hasConflict = false;
 
         oneTasks = Integer.parseInt(st.nextToken());
         repTasks = Integer.parseInt(st.nextToken());
 
-        while (oneTasks + repTasks > 0) {
+        //terminating line will contain 0 0
+        while (!(oneTasks == 0 && repTasks == 0)) {
 
-            //process one limit tasks
+            //process one time tasks
             for (int i = 0; i < oneTasks; i ++) {
+
                 st = new StringTokenizer(in.readLine());
                 start = Integer.parseInt(st.nextToken());
                 end = Integer.parseInt(st.nextToken());
-                schedule.flip(start, end);
-                c = schedule.get(start, end).cardinality();
-                if (c != end - start) { //everything should be ones
-                    hasConflict = true;
-                    break;
+
+                //make sure we're not supposed to terminate
+                if (start == 0 && end == 0) {
+                    printResults(hasConflict);
+                    return;
                 }
 
-            }
-            if (!hasConflict) { //no point in starting if there's already a conflict
-                //process multiple tasks
-                for (int i = 0; i < repTasks; i++) {
-                    st = new StringTokenizer(in.readLine());
-                    start = Integer.parseInt(st.nextToken());
-                    end = Integer.parseInt(st.nextToken());
-                    repInt = Integer.parseInt(st.nextToken());
+                if (!hasConflict) //see notes for justification
+                    if (hasTime(start, end))
+                        schedule.set(start, end);
+                    else
+                        hasConflict = true;
 
-                    while (end < limit) {
-                        schedule.flip(start, end);
-                        c = schedule.get(start, end).cardinality();
-                        if (c != end - start) { //everything should be ones
-                            hasConflict = true;
-                            break;
-                        }
-                        start += repInt;
-                        end += repInt;
-                    }
+            }
+
+
+            //process multiple tasks
+            for (int i = 0; i < repTasks; i++) {
+                st = new StringTokenizer(in.readLine());
+                start = Integer.parseInt(st.nextToken());
+                end = Integer.parseInt(st.nextToken());
+                interval = Integer.parseInt(st.nextToken());
+
+                //make sure we're not supposed to terminate
+                if ((start == 0 && end == 0) || (end == 0 && interval == 0)) {
+                    printResults(hasConflict);
+                    return;
+                }
+
+                //no point in checking each occurrence if there's already a conflict
+                if (hasConflict)
+                    continue;
+                while (end < limit) {
+                    if (hasTime(start, end))
+                        schedule.set(start, end);
+                    else
+                        hasConflict = true;
+                    start += interval;
+                    end += interval;
                 }
             }
 
-            if (hasConflict) {
-                System.out.println("CONFLICT");
-            } else {
-                System.out.println("NO CONFLICT");
-            }
+            printResults(hasConflict);
 
             //get ready for next case
             st = new StringTokenizer(in.readLine());
             oneTasks = Integer.parseInt(st.nextToken());
             repTasks = Integer.parseInt(st.nextToken());
+
             schedule.clear();
             hasConflict = false;
         }
+    }
+
+    private static boolean hasTime(int start, int end) {
+        return schedule.get(start, end).isEmpty();
+    }
+
+    private static void printResults(boolean hasConflict) {
+        if (hasConflict)
+            System.out.println("CONFLICT");
+        else
+            System.out.println("NO CONFLICT");
     }
 }
